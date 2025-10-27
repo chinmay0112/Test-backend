d01d75c00870053851a61b4af24b8574b79ac0b2
Chinmay <chinmaykulshreshtha@gmail.com>
2025-10-27 11:15:56 +0530

    Basic Django Code

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 # core/urls.py
 from django.urls import path
-from .views import TestSeriesListView,TestDetailView
+from .views import TestSeriesListView,TestDetailView,SubmitTestView
 
 urlpatterns = [
     path('test-series/', TestSeriesListView.as_view()),
+    path('tests/<int:pk>/submit/', SubmitTestView.as_view(), name='submit-test'),
     path('tests/<int:pk>/', TestDetailView.as_view()),
+        
+
 
 
 
